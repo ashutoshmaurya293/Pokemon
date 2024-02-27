f19757fef8277ec2fede9acae102d8d5c28fa812
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2024-02-27 12:35:45 +0530

    PokemopnNotFound page added

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -4,7 +4,6 @@ import UseSingleImage from "../Hooks/UseSingleImage";
 import SingleImage from "../singleImage/SingleImage";
 import PokemopnNotFound from "../PokemonNotFound/PokemopnNotFound";
 
-<<<<<<< HEAD
 const Details = ({ pokemonName }) => {
   const mainName = pokemonName?.toLowerCase();
   const { id } = useParams();
@@ -18,15 +17,6 @@ const Details = ({ pokemonName }) => {
       )}
     </div>
   );
-=======
-const Details = ({pokemonName}) => {
-  const mainName = pokemonName.toLowerCase()
-  const { id } = useParams();
-  const { Data,isLoading } = UseSingleImage(id,mainName);
-  return <div>
-    <SingleImage Data = {Data} isLoading={isLoading}/>
-  </div>;
->>>>>>> 869f70e20f726155578e5ac0f9becf480f897952
 };
 
 export default Details;
