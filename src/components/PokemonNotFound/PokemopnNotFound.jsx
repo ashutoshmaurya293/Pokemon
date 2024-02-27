d62972a3b82610e1cdef881846d83ff3f28b1d48
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2024-02-27 15:28:22 +0530

    PokemopnNotFound styling completed

--- src/components/PokemonNotFound/PokemopnNotFound.jsx
+++ src/components/PokemonNotFound/PokemopnNotFound.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import "./PokemopnNotFound.css"
+import React from "react";
+import "./PokemopnNotFound.css";
 import { Link } from "react-router-dom"; // Import Link for navigation
 
-const PokemopnNotFound = ({setsearch,search}) => {
+const PokemopnNotFound = ({ setsearch, search }) => {
   return (
-    <div className='container'>
-      <h1 className='heading'>404 - Page Not Found</h1>
-      <p className='text'>
-        Oops! {search} is not found in our list.
-      </p>
-      <p className='text'>
-        Let's get you back on track. You can either go back to the{" "}
-        <Link to="/"className='link' onClick={()=>setsearch()}>
+    <div className="container">
+      <h1 className="heading">404 - Page Not Found</h1>
+      <p className="text">Oops! "{search}" is not found in our list.</p>
+      <p className="text">
+        Let's get you back on track. You can either go back to the
+        <Link to="/" className="link" onClick={() => setsearch()}>
           homepage
-        </Link>{" "}
+        </Link>
         or search more pokemon.
       </p>
     </div>
   );
 };
 
-export default PokemopnNotFound
+export default PokemopnNotFound;
