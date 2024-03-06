d958319dc553e7e9eb9e5403768880693bf9f595
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2024-03-06 20:45:42 +0530

    new arrow added

--- src/components/morePokemon/MorePokemon.jsx
+++ src/components/morePokemon/MorePokemon.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Usefeath from "../Usefeath";
 import Image from "../Image/Image";
 import "./MorePokemon.css";
+import { FaArrowRight } from "react-icons/fa";
 
 const MorePokemon = ({ type }) => {
   const { pokemonList } = Usefeath(`https://pokeapi.co/api/v2/type/${type}`);
@@ -9,21 +10,10 @@ const MorePokemon = ({ type }) => {
   const slicePokemon = pokemonList.slice(0, 8);
   return (
     <>
-      <h1>Similar type of pokemons</h1><svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="arrow"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-        />
-      </svg>
-
+      <div className="similer">
+        <h1>Similar type of pokemons</h1>
+        <FaArrowRight />
+      </div>
       <div className="pokemons">
         {slicePokemon.map((e) => {
           return (
