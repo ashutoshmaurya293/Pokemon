6f3a3cf22cc0c46074d3661e64a9225ee83600db
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2024-03-09 09:41:15 +0530

    fixed styling

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -5,15 +5,15 @@ import SingleImage from "../singleImage/SingleImage";
 import PokemopnNotFound from "../PokemonNotFound/PokemopnNotFound";
 import GoHome from "../goHome/GoHome";
 
-const Details = ({ pokemonName,setsearch,search }) => {
+const Details = ({ pokemonName, setsearch, search }) => {
   const mainName = pokemonName?.toLowerCase();
   const { id } = useParams();
   const { Data, isLoading, DataNotFound } = UseSingleImage(id, mainName);
   return (
     <div>
-      <GoHome/>
+      {search?.length > 1 ? "" : <GoHome />}
       {DataNotFound ? (
-        <PokemopnNotFound setsearch ={setsearch} search={search}/>
+        <PokemopnNotFound setsearch={setsearch} search={search} />
       ) : (
         <SingleImage Data={Data} isLoading={isLoading} />
       )}
